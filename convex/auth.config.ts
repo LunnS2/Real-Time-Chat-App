88c2465d8d5d872275cb248075130ffb86139165
Lunn <thafm.work@gmail.com>
2024-10-04 17:39:34 +0100

    Delete schema and tables created for learning purposes; Set up Clerk for authentification; Update Schema and Tables; Implement webhooks to enable communication between Convex and Clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://supreme-hedgehog-35.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
