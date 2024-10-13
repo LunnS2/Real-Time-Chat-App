aec4f4023b8336ab2374a2d447d9e7ad11773b00
Lunn <thafm.work@gmail.com>
2024-10-13 23:14:47 +0100

    Add SendImage and SendVideo mutations

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rightful-parakeet-354.convex.cloud',
+        port: '', // leave empty for default
+        pathname: '/api/storage/**', // allows access to all images under /api/storage
+      },
+    ],
+  },
+};
 
 export default nextConfig;
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="m-5">
       <div className="flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel">
         {/* Green background decorator for Light Mode */}
-        <div className="fixed top-0 left-0 w-full h-36 bg-indigo-primary dark:bg-transparent -z-30" />
+        <div className="fixed top-0 left-0 w-full h-28 bg-indigo-primary dark:bg-transparent -z-30" />
         <LeftPanel />
         <RightPanel />
       </div>
