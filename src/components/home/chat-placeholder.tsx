103452253ead4c056dbef280c8d31ef3aa6bb0d6
Lunn <thafm.work@gmail.com>
2025-02-11 20:40:14 +0000

    Update Logo and Name

--- src/components/home/chat-placeholder.tsx
+++ src/components/home/chat-placeholder.tsx
@@ -6,8 +6,8 @@ const ChatPlaceHolder = () => {
 	return (
 		<div className='w-3/4 bg-gray-secondary flex flex-col items-center justify-center py-10'>
 			<div className='flex flex-col items-center w-full justify-center py-10 gap-4'>
-				<Image src={"/desktop-hero.png"} alt='Hero' width={320} height={188} />
-				<p className='text-3xl font-extralight mt-5 mb-2'>Lechat</p>
+				<Image src={"/logo SVG - 6 colors tracing.svg"} alt='Hero' width={320} height={188} />
+				<p className='text-3xl font-extralight mt-5 mb-2'>Safetalk</p>
 				<p className='w-1/2 text-center text-gray-primary text-sm text-muted-foreground'>
           Real-Time Conversations, Real Connections
 				</p>
