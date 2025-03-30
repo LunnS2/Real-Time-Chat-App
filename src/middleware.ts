9a434ea5a61f86132bf30161f150d3f35ed92b78
Lunn <thafm.work@gmail.com>
2025-03-30 22:36:50 +0100

    Add optional middleware for testing

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,3 +27,22 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 }
+
+{/*
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// Middleware function to protect all routes, including "/"
+export default clerkMiddleware(async (auth) => {
+  await auth.protect(); // Protect all routes
+});
+
+// Configuration for the middleware
+export const config = {
+  matcher: [
+    // Match all paths including "/" while skipping Next.js internals and static files
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|json)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
+*/}
