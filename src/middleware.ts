3d2ec0fdf52e34f8c962238137cf3ba17b4d9fb2
Lunn <thafm.work@gmail.com>
2025-03-31 23:50:19 +0100

    Testing middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,21 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
+})
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+}
+
+{/*import { clerkMiddleware } from '@clerk/nextjs/server'
 
 export default clerkMiddleware(async (auth, req) => {
   // Public routes
@@ -28,7 +45,7 @@ export const config = {
   ],
 }
 
-{/*
+
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 // Middleware function to protect all routes, including "/"
