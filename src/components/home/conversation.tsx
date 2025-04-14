79fc8d04f359486a3dc09fbb55d86d15ba81cfcc
Lunn <thafm.work@gmail.com>
2025-04-14 16:58:56 +0100

    Fix: 1 on 1 chat user images are now displaying; Initalized cancel conversation creation functionality

--- src/components/home/conversation.tsx
+++ src/components/home/conversation.tsx
@@ -31,9 +31,8 @@ interface ConversationProps {
 }
 
 const Conversation = ({ conversation }: ConversationProps) => {
-  const conversationImage = conversation.groupImage;
-  const conversationName =
-    conversation.groupName || conversation.name || "New Chat";
+  const conversationImage = conversation.groupImage || conversation.image;
+  const conversationName = conversation.groupName || conversation.name || "New Chat";
   const lastMessage = conversation.lastMessage;
 
   const { isAuthenticated } = useConvexAuth();
